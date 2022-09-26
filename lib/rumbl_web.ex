f70ccc27d16c6adf1c4ca974052b7bc4185a7cc3
prvladko <prvladko@gmail.com>
2022-09-26 10:04:41 +0300

    Using Helpers

--- lib/rumbl_web.ex
+++ lib/rumbl_web.ex
@@ -37,6 +37,13 @@ defmodule RumblWeb do
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
+      # Use all HTML functionality (forms, tags, etc)​
+      # use Phoenix.HTML
+
+      import RumblWeb.ErrorHelpers
+      import RumblWeb.Gettext
+      alias RumblWeb.Router.Helpers, as: Routes
+
       # Include shared imports and aliases for views
       unquote(view_helpers())
     end
