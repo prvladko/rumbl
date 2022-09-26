bff8bf616d6119865aaa15ffed05937d94be9c06
prvladko <prvladko@gmail.com>
2022-09-26 18:22:31 +0300

    Using the Repository to Add Data

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -3,24 +3,24 @@ defmodule Rumbl.Accounts do
   The Account context.
   """
 
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "Vlad", username: "vladko"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
+  def get_user(id) do
+    # Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+  def get_user!(id) do
+    # Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
 
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+  def list_users do
+    Repo.all(User)
   end
 end
